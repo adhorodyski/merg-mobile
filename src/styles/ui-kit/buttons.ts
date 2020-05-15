643d5198dcb9d5abe820d2722330b05a354e635f
Adam Horodyski <ad.horodyski@gmail.com>
2020-05-15 10:47:20 +0200

    feature/rebuild WIP remove unused files

--- src/styles/ui-kit/buttons.ts
+++
@@ -1,7 +0,0 @@
-import React, { PureComponent } from 'react';
-import { View, Text, Image, TouchableHighlight, Dimensions } from 'react-native';
-import styled from 'styled-components';
-import posed from 'react-native-pose';
-import { Ionicons } from '@expo/vector-icons';
-import { palette } from '../palette';
-import * as theme from './themes';
