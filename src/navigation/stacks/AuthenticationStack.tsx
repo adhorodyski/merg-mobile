0739f6b3d516b1f1ee20d3bad1d47fbe5fde1277
Adam Horodyski <ad.horodyski@gmail.com>
2020-05-18 22:12:18 +0200

    feature/rebuild add signin screen to the auth stack

--- src/navigation/stacks/AuthenticationStack.tsx
+++ src/navigation/stacks/AuthenticationStack.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Landing } from 'views/Landing';
+import { SignIn } from 'views/SignIn';
 
 const Stack = createStackNavigator();
 
@@ -8,6 +9,7 @@ export const AuthenticationStack: React.FC = () => {
     return (
         <Stack.Navigator initialRouteName="Landing" headerMode="none">
             <Stack.Screen name="Landing" component={Landing} />
+            <Stack.Screen name="SignIn" component={SignIn} />
         </Stack.Navigator>
     );
 };
